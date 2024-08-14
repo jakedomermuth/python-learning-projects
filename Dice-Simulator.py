a075f3962343421279a847ddf02087b96cf118d8
Jake Domermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-08-14 16:01:21 -0400

    Add files via upload

---
+++ Dice-Simulator.py
@@ -0,0 +1,36 @@
+from random import randint
+
+MIN_ROLL = 1
+MAX_ROLL = 6
+
+def amount_of_dice():
+    while True:
+        dice = input('How many dice would you like to roll? (Type "quit" to exit) ')
+        if dice.lower() == 'quit':
+            return 'quit'
+        elif dice.isdigit():
+            dice = int(dice)
+            if dice > 0:
+                return dice
+            else:
+                print('The amount of dice must be greater than 0')
+        else:
+            print('Please enter a valid number of dice')
+
+def main():
+    while True:
+        dice_to_roll = amount_of_dice()
+        if dice_to_roll == 'Quit':
+            print('')
+            break
+        else:
+            for i in range(dice_to_roll):
+                random_roll = randint(MIN_ROLL, MAX_ROLL)
+                print(random_roll, end=' ')
+            print()
+
+main()
+
+
+
+
